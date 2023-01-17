e65112ef4e482055a957076dee76fc5beca3017d
sangyeob Lee <ydeja6@gmail.com>
2023-01-17 17:49:17 +0900

    [MODIFY] DBmigration

--- db/migrations/20230114061738_create_dates_table.sql
+++ db/migrations/20230114061738_create_dates_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE date (
     main_img TEXT,
     user_id INT NOT NULL,
     opentime TIME,
-    clodetime TIME,
+    closetime TIME,
     desciption TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
